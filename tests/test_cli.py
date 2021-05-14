5bd1efd390c5788a9d53d2264cd1981c7a845671
gabicca <gabriella.hodosan@stfc.ac.uk>
2021-05-14 12:00:18 +0100

    SIM-873: update original cli tests to look for logs instead of stdout messages

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,63 +15,61 @@ from tests.test_backend_etcd3 import PREFIX as etcd_prefix
 # fixture, do not delete
 from tests.test_backend_etcd3 import etcd3
 
-
 PREFIX = "/__test_cli"
 
 
-def _test_cli_command(capsys, argv,
-                      expected_stdout=None, expected_stderr=None):
+def _test_cli_command(argv, message=None):
+    with patch("logging.Logger.info") as mock_log:
+        cli.main(argv)
 
-    cli.main(argv)
-    out, err = capsys.readouterr()
-    if expected_stdout is not None:
-        assert out == expected_stdout
-    if expected_stderr is not None:
-        assert err == expected_stderr
+    if isinstance(message, str):
+        mock_log.assert_called_with(message)
+
+    elif isinstance(message, list):
+        mock_log.assert_has_calls(message)
 
 
 def _delete_test_pb():
     cfg = config.Config(global_prefix=PREFIX)
     for txn in cfg.txn():
-        txn.raw.delete(PREFIX)
+        for key in txn.raw.list_keys(PREFIX, recurse=8):
+            txn.raw.delete(key)
 
 
-def test_cli_simple(capsys):
-
-    if os.getenv("SDP_TEST_HOST") is not None:
-        os.environ["SDP_CONFIG_HOST"] = os.getenv("SDP_TEST_HOST")
-
-    _test_cli_command(capsys, ['delete', '-R', PREFIX])
-
-    _test_cli_command(capsys, ['get', PREFIX+'/test'], PREFIX+"/test = None\n", "")
-    _test_cli_command(capsys, ['create', PREFIX+'/test', 'asdf'], "OK\n", "")
-    _test_cli_command(capsys, ['get', PREFIX+'/test'], PREFIX+"/test = asdf\n", "")
-    _test_cli_command(capsys, ['update', PREFIX+'/test', 'asd'], "OK\n", "")
-    _test_cli_command(capsys, ['get', PREFIX+'/test'], PREFIX+"/test = asd\n", "")
-    _test_cli_command(capsys, ['-q', 'get', PREFIX+'/test'], "asd\n", "")
-    _test_cli_command(capsys, ['delete', PREFIX+'/test'], "OK\n", "")
-
-    _delete_test_pb()
-
-
-def test_cli_simple2(capsys):
+def test_cli_simple():
 
     if os.getenv("SDP_TEST_HOST") is not None:
         os.environ["SDP_CONFIG_HOST"] = os.getenv("SDP_TEST_HOST")
 
-    _test_cli_command(capsys, ['create', PREFIX+'/test', 'asdf'], "OK\n", "")
-    _test_cli_command(capsys, ['create', PREFIX+'/foo', 'bar'], "OK\n", "")
-    _test_cli_command(capsys, ['ls', PREFIX+'/'],
-                      "Keys with {pre}/ prefix:\n{pre}/foo\n{pre}/test\n".format(
-                          pre=PREFIX), "")
-    _test_cli_command(capsys, ['-q', 'list', PREFIX+'/'],
-                      "{pre}/foo {pre}/test\n".format(pre=PREFIX),
-                      "")
-    _test_cli_command(capsys, ['--prefix', PREFIX, 'process', 'realtime:test:0.1'],
-                      "OK, pb_id = pb-sdpcfg-{}-00000\n".format(
-                          date.today().strftime('%Y%m%d')), "")
-    _test_cli_command(capsys, ['delete', PREFIX+'/test'], "OK\n", "")
-    _test_cli_command(capsys, ['delete', PREFIX+'/foo'], "OK\n", "")
+    _test_cli_command(['delete', '-R', PREFIX])
+
+    _test_cli_command(['get', PREFIX+'/test'], PREFIX+"/test = None")
+    _test_cli_command(['create', PREFIX+'/test', 'asdf'], "OK")
+    _test_cli_command(['get', PREFIX+'/test'], PREFIX+"/test = asdf")
+    _test_cli_command(['update', PREFIX+'/test', 'asd'], "OK")
+    _test_cli_command(['get', PREFIX+'/test'], PREFIX+"/test = asd")
+    _test_cli_command(['-q', 'get', PREFIX+'/test'], "asd")
+    _test_cli_command(['delete', PREFIX+'/test'], "OK")
+
+
+def test_cli_simple2():
+
+    if os.getenv("SDP_TEST_HOST") is not None:
+        os.environ["SDP_CONFIG_HOST"] = os.getenv("SDP_TEST_HOST")
+
+    _test_cli_command(['create', PREFIX+'/test', 'asdf'], "OK")
+    _test_cli_command(['create', PREFIX+'/foo', 'bar'], "OK")
+    _test_cli_command(['ls', PREFIX+'/'],
+                      [call(f"Keys with {PREFIX}/ prefix:"),
+                       call(f"{PREFIX}/foo"),
+                       call(f"{PREFIX}/test")])
+    _test_cli_command(['-q', 'list', PREFIX+'/'],
+                      "{pre}/foo {pre}/test".format(pre=PREFIX))
+    _test_cli_command(['--prefix', PREFIX, 'process', 'realtime:test:0.1'],
+                      "OK, pb_id = pb-sdpcfg-{}-00000".format(
+                          date.today().strftime('%Y%m%d')))
+    _test_cli_command(['delete', PREFIX+'/test'], "OK")
+    _test_cli_command(['delete', PREFIX+'/foo'], "OK")
 
     _delete_test_pb()
 
